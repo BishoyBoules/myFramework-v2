407ed1306541a6e418d4fd392b6cf1e59e7f721e
Bishoy Boules <BishoyBoules@github>
2022-02-10 15:51:08 +0200

    update javascript file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 const head = document.head;
-
-let link = document.createElement('link');
-link.type = "text/css";
-link.rel = "stylesheet";
-head.appendChild(link);
-
+const link = document.querySelector('[data-type]');
 function colorChange(id) {
     link.href = `css/themes/${id}.css`
 }
